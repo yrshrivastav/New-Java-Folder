90a5b944545faa320e3493fa6ffab729bd3c6152
yrshrivastav <yrshrivastav42+github@gmail.com>
2024-01-13 00:38:12 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Welcome to VS Code Editor!");
+    }
+}
